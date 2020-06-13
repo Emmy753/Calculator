edc03946aa57d3485414b44c6cf63a8b433e3025
pointech <ojoaduragbemi999@gmail.com>
2020-06-12 21:06:41 -0700

    this is another repository

---
+++ TipCalculator.js
@@ -0,0 +1,46 @@
+//calculator Tip
+function calculatorTip() {
+var billAmt = document.getElementById("billamt").value;
+var serviceQual =
+document.getElementById("serviceQual").value;
+var numOfPeople =
+document.getElementById("peopleamt").value;
+
+
+//validate input
+if (billAmt === "" || serviceQual == 0) {
+    alert("Please enter values");
+    return;
+}
+    
+ // check to see if this input is empty or less than or equal to 1
+    if (numOfPeople === "" || numOfPeople <= 1) {
+        numOfPeople = 1;
+        document.getElementById("each").style.display = "none";
+    } else {
+        document.getElementById("each").style.display = "block";
+    }
+    
+    // calculate Tip
+    var total = (billAmt * serviceQual) / numOfPeople;
+    // round tp two decimal places
+    total = Math.round(total * 100) / 100;
+    //next line allows us to always have two digits after decimal point
+    total= total.toFixed(2);
+    //Display the tip
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+
+
+}
+
+// Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// click to call function
+
+var btn = document.getElementById("calculate");
+btn.addEventListener("click", function(){
+    calculatorTip();
+});
